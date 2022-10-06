a4bc18b91428efdcc189492d8b29f9334882879d
Mel Incognito <melxincognito@gmail.com>
2022-10-06 15:34:19 -0700

    initial commit

---
+++ pages/going.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Going: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title> put</title>
+      </Head>
+      put
+    </>
+  );
+};
+
+export default Going;
