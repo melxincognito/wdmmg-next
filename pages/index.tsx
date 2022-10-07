826f07b7855f38f30aef22abf16732cd638ae938
Mel Incognito <melxincognito@gmail.com>
2022-10-06 18:33:00 -0700

    made nice and pretty

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,20 +20,39 @@ const Home: NextPage = () => {
     setPurchaseAmount(parseFloat(e.target.value));
   };
 
+  const resetForm = () => {
+    setPurchaseName("");
+    setPurchaseAmount(0);
+  };
+
   const submitPurchase = (e: any) => {
     e.preventDefault();
 
-    setPurchases((prevPurchases) => [
-      ...prevPurchases,
-      {
-        name: purchaseName,
-        amount: purchaseAmount,
-        index: purchases.length + 1,
-      },
-    ]);
+    if (purchaseName === "" && purchaseAmount === 0) {
+      alert("Please fill all input fields");
+      return;
+    } else if (purchaseName === "") {
+      alert("Please fill purchase name input field");
+    } else if (purchaseAmount === 0 || null || undefined) {
+      alert("Please fill purchase amount input field");
+    } else {
+      let index = purchases.length + 1;
+
+      setPurchases((prevPurchases) => [
+        ...prevPurchases,
+        {
+          name: purchaseName,
+          amount: purchaseAmount,
+          index: index,
+        },
+      ]);
+
+      resetForm();
+    }
   };
 
   const removeFromPurchases = (purchaseIndex: number) => {
+    console.log(purchaseIndex);
     setPurchases(
       purchases.filter((purchase) => purchase.index !== purchaseIndex)
     );
@@ -52,42 +71,69 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1>Where did my money go?</h1>
-        <form onSubmit={submitPurchase} style={{ display: "grid" }}>
-          <label htmlFor="purchase-name-input">Purchase Name</label>
-          <input
-            id="purchase-name-input"
-            className={styles.purchaseInput}
-            placeholder="Purchase Name"
-            type="text"
-            onChange={handleChangePurchaseName}
-            aria-label="Purchase name input"
-          />
-          <label htmlFor="purchase-amount-input"> Purchase Amount</label>
-          <input
-            id="purchase-amount-input"
-            className={styles.purchaseInput}
-            placeholder="Purchase Amount"
-            type="number"
-            step=".01"
-            onChange={handleChangePurchaseAmount}
-            aria-label="Purchase amount input"
-          />
-          <button type="submit"> Submit Purchase</button>
+        <form
+          onSubmit={submitPurchase}
+          style={{ display: "grid", gap: "1rem" }}
+        >
+          <div className={styles.formInputsDiv}>
+            <div className={styles.formControl}>
+              <label htmlFor="purchase-name-input">Purchase Name</label>
+              <input
+                id="purchase-name-input"
+                className={styles.purchaseInput}
+                placeholder="Purchase Name"
+                type="text"
+                onChange={handleChangePurchaseName}
+                aria-label="Purchase name input."
+                value={purchaseName}
+              />
+            </div>
+            <div className={styles.formControl}>
+              <label htmlFor="purchase-amount-input"> Purchase Amount</label>
+              <input
+                id="purchase-amount-input"
+                className={styles.purchaseInput}
+                placeholder="Purchase Amount"
+                value={purchaseAmount}
+                type="number"
+                step=".01"
+                onChange={handleChangePurchaseAmount}
+                aria-label="Purchase amount input."
+              />
+            </div>
+          </div>
+
+          <button type="submit" className={styles.submitPurchaseButton}>
+            {" "}
+            Submit Purchase
+          </button>
         </form>
-        <div className={styles.purchasesList}>
+        <div className={styles.purchasesList} aria-live="assertive">
           {purchases.length > 0 ? (
             <>
-              {purchases.map((purchase, index) => (
-                <div key={index} className={styles.purchaseTile}>
-                  <button onClick={() => removeFromPurchases(index)}>X</button>
-                  <p>
-                    {" "}
-                    <u>Purchase:</u> {purchase.name}
-                  </p>
-                  <p>
-                    {" "}
-                    <u>Amount:</u> ${purchase.amount}
-                  </p>
+              {purchases.map((purchase) => (
+                <div key={purchase.index} className={styles.purchaseTile}>
+                  <span className={styles.purchaseTileDecorSpan}></span>
+                  <span className={styles.purchaseTilePurchasesSpan}>
+                    <p>
+                      {" "}
+                      <u>Purchase:</u> {purchase.name}
+                    </p>
+                  </span>
+                  <span className={styles.purchaseTileAmountSpan}>
+                    <p>
+                      {" "}
+                      <u>Amount:</u> ${purchase.amount}
+                    </p>
+                  </span>
+
+                  <button
+                    onClick={() => removeFromPurchases(purchase.index)}
+                    className={styles.purchaseTileButton}
+                    aria-label="Delete purchase."
+                  >
+                    X
+                  </button>
                 </div>
               ))}
             </>
@@ -96,19 +142,6 @@ const Home: NextPage = () => {
           )}
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
   );
 };
