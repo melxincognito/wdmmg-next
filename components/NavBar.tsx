cbb62567a00067b3aaf9c93b25b824fe74e05834
Mel Incognito <melxincognito@gmail.com>
2022-10-06 22:33:05 -0700

    created navbar component

---
+++ components/NavBar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "../styles/NavBar.module.css";
+
+export const NavBar = () => {
+  return (
+    <nav className={styles.navBar}>
+      <h1>¿ Where did my money go ?</h1>
+    </nav>
+  );
+};
